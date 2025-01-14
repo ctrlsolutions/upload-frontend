24a3328e16bd468185e8925e58d7eafa7aed7472
Az <arbusinos@up.edu.ph>
2025-01-14 12:13:12 +0800

    [fix][az] moved the routing around

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SignupPage from '@/pages/SignupPage.vue'
+import SignUpPage from '@/pages/SignUpPage.vue'
+import LogInPage from '@/pages/LogInPage.vue'
 import AuthPagesLayout from '@/layouts/AuthPagesLayout.vue'
 import EmptyPagesLayout from '@/layouts/EmptyPagesLayout.vue'
+import LandingPage from '@/pages/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: '',
       component: EmptyPagesLayout,
     },
+
+    {
+      path: '/landing',
+      name: 'landing',
+      component: LandingPage,
+    },
+
     {
       path: '/auth',
       name: 'auth',
       component: AuthPagesLayout,
-      children: [
-        { path: 'signup', component: SignupPage}
-      ]
+      children: [{ path: 'signup', component: SignUpPage }],
     },
-  
+
     {
       path: '/about',
       name: 'about',
@@ -29,10 +36,16 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
     },
     {
-      path:'/signup',
-      name: 'singup',
-      component: SignupPage,
-    }
+      path: '/signup',
+      name: 'signup',
+      component: SignUpPage,
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: LogInPage,
+    },
   ],
 })
 
