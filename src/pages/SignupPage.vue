d73ea83a6e99ac593e0a19b9ef54f5cb86b4c5a6
Az <arbusinos@up.edu.ph>
2025-01-14 12:18:35 +0800

    [chore][az] remove old SignupPage.vue

--- src/pages/SignupPage.vue
+++ src/pages/SignupPage.vue
@@ -1,17 +1,15 @@
 <template>
-    <div :class="$style.container">
-        <h1>subcomponent</h1>
-    </div>
+  <div :class="$style.container">
+    <h1>subcomponent</h1>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style module>
-    .container{
-        border: 1px solid black;
-        height: 80vh;
-        flex-grow: 0.9;
-        
-    }
+.container {
+  border: 1px solid black;
+  height: 80vh;
+  flex-grow: 0.9;
+}
 </style>
