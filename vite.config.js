87abb876eab5923b381ec984e7c7eb585edb10a9
Az <arbusinos@up.edu.ph>
2025-01-14 12:03:43 +0800

    [fix][az] Updated deprecated @import to @forward and @use

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables.scss";`,
+        additionalData: `@use "@/styles/globals.scss" as *;`,
       },
     },
   },
