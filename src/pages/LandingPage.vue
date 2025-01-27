6f2a6a438e2eeea40415adf54882fe80595a228e
Lanieza <lblopez@up.edu.ph>
2025-01-27 09:46:01 +0800

    fixed merge histories of feat/landing-page-home and feat/checkbox-component

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -10,6 +10,79 @@
       <img src="@/assets/Oble.png" class="bg-image" />
     </div>
   </div>
+  <div class="name1">
+      <!-- Circle Checkboxes: Only one can be selected at a time in name1 -->
+      <BaseFormCheckbox
+        id="terms"
+        label="Choice 1"
+        :checked="circleSelected1 === 'terms'"
+        @change="selectCircle('terms', 'name1')"
+        variant="circle"
+      />
+      <BaseFormCheckbox
+        id="name2"
+        label="Choice 2"
+        :checked="circleSelected1 === 'name2'"
+        @change="selectCircle('name2', 'name1')"
+        variant="circle"
+      />
+      <BaseFormCheckbox
+        id="name3"
+        label="Choice 3"
+        :checked="circleSelected1 === 'name3'"
+        @change="selectCircle('name3', 'name1')"
+        variant="circle"
+      />
+    </div>
+
+    <!-- New Class for Another Set of Checkboxes (name2) -->
+    <div class="name2">
+      <!-- New Circle Checkboxes: Only one can be selected at a time in name2 -->
+      <BaseFormCheckbox
+        id="option1"
+        label="New Choice 1"
+        :checked="circleSelected2 === 'option1'"
+        @change="selectCircle('option1', 'name2')"
+        variant="circle"
+      />
+      <BaseFormCheckbox
+        id="option2"
+        label="New Choice 2"
+        :checked="circleSelected2 === 'option2'"
+        @change="selectCircle('option2', 'name2')"
+        variant="circle"
+      />
+      <BaseFormCheckbox
+        id="option3"
+        label="New Choice 3"
+        :checked="circleSelected2 === 'option3'"
+        @change="selectCircle('option3', 'name2')"
+        variant="circle"
+      />
+    </div>
+
+    <!-- New Class for Square Checkbox -->
+    <div class="name3">
+      <!-- Square Checkbox -->
+      <BaseFormCheckbox
+        id="square1"
+        label="Square Option 1"
+        v-model="squareSelected1"
+        variant="square"
+      />
+      <BaseFormCheckbox
+        id="square2"
+        label="Square Option 2"
+        v-model="squareSelected2"
+        variant="square"
+      />
+      <BaseFormCheckbox
+        id="square3"
+        label="Square Option 3"
+        v-model="squareSelected3"
+        variant="square"
+      />
+    </div>
 </template>
 
 <script>
