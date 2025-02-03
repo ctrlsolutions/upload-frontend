3eda6d7413bc9df32894a991b2605878b79053c6
ndb3llo <ndbello@up.edu.ph>
2025-02-03 11:57:10 +0800

    [feat][Nico] Made the ABOUT bold and made the allignment leaning to left

--- src/components/Landing/About.vue
+++ src/components/Landing/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="about-container">
     <h1 class="title">ABOUT</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -16,23 +16,20 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  height: 100vh;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: Arial, sans-serif;
-}
-
-.container {
-  text-align: center;
-  font-weight: bold;
+.about-container {
+  text-align: left;
+  font-weight: 1200;
+  font-family: 'Inter', serif;
+  max-width: 900px; 
+  margin: 0 auto; 
+  padding: 30px; 
 }
 
 .title {
   font-size: 2.5rem;
-  font-weight: bold;
+  font-weight: 800;
   color: maroon;
+  font-family: 'Inter', serif;
+  margin-bottom: 10px; 
 }
 </style>
