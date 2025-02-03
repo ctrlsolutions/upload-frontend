4e69f8f04d1e13b5bab599e3e3e06aaa7844c37f
ndb3llo <ndbello@up.edu.ph>
2025-02-03 10:49:10 +0800

    [feat][Nico] Created about section temporarily

--- src/components/Landing/About.vue
+++ src/components/Landing/About.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="container">LANDING Summary</div>
+  <div class="container">
+    <h1 class="title">ABOUT</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
 </template>
-<script></script>
+
+<script>
+export default {
+  name: "AboutSection",
+};
+</script>
+
 <style lang="scss">
 body {
   height: 100vh;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Arial, sans-serif;
 }
+
 .container {
-  height: 100vh;
+  text-align: center;
+  font-weight: bold;
+}
+
+.title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: maroon;
 }
 </style>
