d2d9306dbf539860bdc98509681f8e43032303ff
ndb3llo <ndbello@up.edu.ph>
2025-02-03 21:25:10 +0800

    [feat][Nico] Did major changes. Used Flexbox. Created a class 'about-content'. Removed redundancies.

--- src/components/Landing/About.vue
+++ src/components/Landing/About.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="about-container">
     <h1 class="title">ABOUT</h1>
-    <p>
+    <p class="about-content">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      
     </p>
   </div>
 </template>
@@ -17,19 +18,32 @@ export default {
 
 <style lang="scss">
 .about-container {
+  display: flex;           
+  flex-direction: column; 
+  align-items: left;      
+  justify-content: left; 
   text-align: left;
-  font-weight: 1200;
+  font-weight: 900;
   font-family: 'Inter', serif;
-  max-width: 900px; 
+  max-width: 56.25rem;
   margin: 0 auto; 
   padding: 30px; 
+  gap: px; /* Adds spacing between elements */
+}
+
+.about-text {
+  font-size: 1.2rem;
+  line-height: 1.6;
+  color: #333;
 }
 
 .title {
-  font-size: 2.5rem;
-  font-weight: 800;
+  font-size: 3rem;
+  font-weight: 900;
   color: maroon;
-  font-family: 'Inter', serif;
-  margin-bottom: 10px; 
+  margin-bottom: 5px; 
 }
+
+
+
 </style>
