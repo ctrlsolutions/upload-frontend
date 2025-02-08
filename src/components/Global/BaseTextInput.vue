08cf5b4baaaf40bddea9de47bbc5119ceb6f14ff
Caleb <calebjosh.berandoy@gmail.com>
2025-02-08 17:11:38 +0800

    [Feat][Caleb] Applied requested changes (removed textStyle() property and getVariantColor() emthod

--- src/components/Global/BaseTextInput.vue
+++ src/components/Global/BaseTextInput.vue
@@ -34,21 +34,7 @@
       variantClass() {
         return `input-box--${this.variant}`
       },
-      textStyle() {
-        return {
-            '--placeholder-color': this.getVariantColor(this.variant),
-        };
-      },
     },
-    methods: {
-        getVariantColor(variant) {
-            const colors = {
-                red: '$red',
-                green: '$green'
-            };
-            return colors[variant] || '#000000';
-        }
-    }
   }
   </script>
   
@@ -60,30 +46,40 @@
     border-radius: 12px;
     font-family: "Inter", serif;
   
-    &--red {
-        border: 0.2em $red;
-        border-radius: 1.2em;
-        border-style: solid;
-        outline: none;
-        font-weight: bold;
-        font-size: medium;
-        color: $red;
-        padding: 1.25em;
-    }
-    
-    &--green {
-        border: 0.25em $green;
-        border-radius: 1.25em;
-        border-style: solid;
-        outline: none;
-        font-weight: bold;
-        color: $green;
-        padding: 1.5em;
-    }
     &::placeholder {
-      color: var(--placeholder-color);
-      opacity: 0.7;
+    color: #999;
+    opacity: 0.7;
+  }
+
+  &--red {
+    border: 0.2em $red;
+    border-radius: 1.2em;
+    border-style: solid;
+    outline: none;
+    font-weight: bold;
+    font-size: medium;
+    color: $red;
+    padding: 1.25em;
+
+    &::placeholder {
+      color: $red;
     }
+  }
+
+  &--green {
+    border: 0.2em $green;
+    border-radius: 1.25em;
+    border-style: solid;
+    outline: none;
+    font-weight: bold;
+    font-size: medium;
+    color: $green;
+    padding: 1.25em;
+
+    &::placeholder {
+      color: $green;
+    }
+  }
   }
   
   </style>
