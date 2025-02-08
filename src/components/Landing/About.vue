af74e60c2eb296072aaa2acd6f5456a8eb5ebb71
ndb3llo <ndbello@up.edu.ph>
2025-02-08 20:45:41 +0800

    [feat][Nico] Updated the boldness of ABOUT title and the placement of the overall about page section.

--- src/components/Landing/About.vue
+++ src/components/Landing/About.vue
@@ -2,9 +2,7 @@
   <div class="about-container">
     <h1 class="title">ABOUT</h1>
     <p class="about-content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie dictum blandit. Vivamus tempus urna ut quam hendrerit rutrum eget sed orci. Nullam tincidunt vitae ligula nec tempor. Maecenas dapibus mauris sed sapien pretium aliquet. Nullam pellentesque pulvinar risus, eu vestibulum nibh consequat blandit. Pellentesque egestas erat sed vestibulum interdum. Aenean accumsan metus aliquet placerat cursus. Cras facilisis lacus ut elit imperdiet, at ornare lacus laoreet. Donec interdum justo nec porta luctus. Pellentesque pretium tempor eros. Curabitur in velit tincidunt, ullamcorper diam et, faucibus eros. Cras fringilla nibh vel diam pretium porttitor. Nullam finibus eu enim non aliquet.
     </p>
   </div>
 </template>
@@ -16,31 +14,33 @@ export default {
 </script>
 
 <style lang="scss">
-
 .about-container {
   display: flex;           
   flex-direction: column; 
   align-items: flex-start;      
-  justify-content: flex-start; 
+  justify-content: center; 
   text-align: left;
-  font-weight: 900;
-  font-family: 'Inter', serif;
+  font-family: 'Inter', sans-serif;
   max-width: 100%;
+  min-height: 100vh; 
   padding: 30px; 
+  padding-left: 90px;
   gap: 10px; 
 
-  //BREAKPOINTS
+  // BREAKPOINTS
   @include sm {
     max-width: 90%;
-    padding: 30px; 
+    padding-left: 90px; 
   }
 
   @include md {
     max-width: 70%;
+    padding-left: 90px; 
   }
 
   @include lg {
     max-width: 56.25rem;
+    padding-left: 90px; 
   }
 }
 
@@ -48,8 +48,9 @@ export default {
   font-size: 1.2rem;
   line-height: 1.6;
   color: #333;
+  max-width: 80%; 
 
-  //BREAKPOINTS
+  // BREAKPOINTS
   @include sm {
     font-size: 1rem;
     line-height: 1.4;
@@ -66,11 +67,11 @@ export default {
 
 .title {
   font-size: 3rem;
-  font-weight: 900;
+  font-weight: 800; 
   color: maroon;
-  margin-bottom: 5px; 
+  margin-bottom: 0px;
 
-  //BREAKPOINTS
+  // BREAKPOINTS
   @include sm {
     font-size: 2rem;
   }
