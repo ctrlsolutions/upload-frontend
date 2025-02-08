b6d058e0088633a4d9f3c03068525c4ac9933cb6
royalsprites ‎ <aaasenoron@gmail.com>
2025-02-08 17:12:26 +0800

    [feat][angel] adjustments in landing home page. also added the UPLoad logo

--- src/components/Landing/Home.vue
+++ src/components/Landing/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="container">
+  <div class="container space-y-4">
     <div class="header-logos">
-      <img class="logo" src="@/assets/UP-logo.svg" />
       <img class="logo" src="@/assets/UP-logo.svg" />
     </div>
     <div class="title">
@@ -10,39 +9,50 @@
     </div>
     <div class="subtitle">Honor, Excellence, Service</div>
     <div class="buttons">
-      <BaseNavButton route="/auth/login" variant="red-full"
-        >Log in</BaseNavButton
-      >
-      <BaseNavButton route="/auth/signup" variant="green-outline"
-        >Sign up</BaseNavButton
-      >
+      <BaseNavButton route="/auth/login" variant="red-full">
+        Log in
+      </BaseNavButton>
+      <BaseNavButton route="/auth/signup" variant="green-outline">
+        Sign up
+      </BaseNavButton>
     </div>
   </div>
 </template>
+
 <script>
-import BaseNavButton from '@/components/Global/BaseNavButton.vue'
+import BaseNavButton from '@/components/Global/BaseNavButton.vue';
 export default {
+  name: 'HomePage',
   components: {
     BaseNavButton,
   },
-}
+};
 </script>
+
 <style lang="scss" scoped>
+.header-logos {
+  display: flex;
+}
 .container {
   background-color: white;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: flex-start;
   min-height: 100vh;
   overflow-y: scroll;
   scroll-behavior: smooth;
   scrollbar-width: none;
-  padding: 25px 0px 30px 30px;
-  gap: 10px;
+  padding: 25px;
+  gap: 14px;
+  text-align: left;
 }
 .title {
-  font-weight: bolder;
+  font-weight: 800;
   color: $red;
+  font-size: 2rem;
+  max-width: 320px;
+  line-height: 2.5rem;
 }
 .subtitle {
   font-size: small;
@@ -50,16 +60,15 @@ export default {
 .header-logos {
   display: flex;
   flex-direction: row;
-  gap: 10px;
 }
 .logo {
-  object-fit: contain;
-  max-width: 20%;
-  max-height: 100%;
+  object-fit:contain;
+  max-width: 70%;
 }
 .buttons {
   display: flex;
   flex-direction: row;
   gap: 10px;
+  margin-top: 20px;
 }
 </style>
