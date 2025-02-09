ffda06b203ed54cbc98b940f66fc85c6a385c357
Erl <136324144+ermontano@users.noreply.github.com>
2025-02-09 09:57:31 +0800

    [Feat][Erl] Commit with new changes

--- src/components/Global/BaseSelectInput.vue
+++ src/components/Global/BaseSelectInput.vue
@@ -11,27 +11,22 @@
 
 <script setup>
 import { defineProps, defineModel, computed } from 'vue';
-
 const props = defineProps({
   id: String,
   placeholder: { type: String, default: 'Select Option' },
   variant: { type: String, default: 'default' },
   options: { type: Array, default: () => ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'] }
 });
-
 const model = defineModel();
 const variantClass = computed(() => `form-dropdown--${props.variant}`);
 </script>
 
 <style lang="scss" scoped>
-@import './assets/scss/variables';
-
 .form-dropdown {
   width: 12.5rem;
   margin: 0 auto;
   padding: 0.9375rem;
   background: transparent;
-
   &__select {
     width: 100%;
     padding: 0.75rem;
@@ -43,6 +38,18 @@ const variantClass = computed(() => `form-dropdown--${props.variant}`);
     outline: none;
     color: $red;
     text-align: center;
+    appearance: none;
+    background-image: linear-gradient(45deg, transparent 50%, $red 50%),
+                      linear-gradient(135deg, $red 50%, transparent 50%),
+                      linear-gradient(90deg, $red 50%, transparent 50%);
+    background-position: calc(100% - 1.25rem) calc(1em + 0.125rem),
+                         calc(100% - 0.9375rem) calc(1em + 0.125rem),
+                         calc(100% - 2.1875rem) 50%;
+    background-size: 0.3125rem 0.3125rem,
+                    0.3125rem 0.3125rem,
+                    0.0625rem 60%;
+    background-repeat: no-repeat;
+    padding-right: 3rem;
   }
 }
 </style>
