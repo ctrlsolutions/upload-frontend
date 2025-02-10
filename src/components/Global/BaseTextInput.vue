2ecbb7072d83ce561142c4811693fa2382619e42
Caleb <calebjosh.berandoy@gmail.com>
2025-02-10 10:51:55 +0800

    [Feat][Caleb] Modified width and height customizability

--- src/components/Global/BaseTextInput.vue
+++ src/components/Global/BaseTextInput.vue
@@ -1,95 +1,105 @@
 <template>
-      <input 
-      :id="id"
-      :type="type" 
-      :class="['input-box',variantClass]" 
-      :style="{ '--input-width': width, '--input-height': height}" 
-      :placeholder="placeholder"
-      v-bind="$attrs"
-      />
+  <input 
+  :id="id"
+  :type="type" 
+  :class="['input-box',variantClass]" 
+  :style="{ width: width, height: height}" 
+  :placeholder="placeholder"
+  v-bind="$attrs"
+  />
 </template>
-  
-  <script>
-  export default {
-    text: String,
-    props: {
-      id: {
-        type: String,
-        required: true,
-      },
-      type: {
-        type: String,
-        default: 'text',
-      },
-      placeholder: {
-        type: String,
-        default: 'Placeholder',
-      },
-      variant: {
-        type: String,
-        default: 'red',
-      },
-      width: {
-        type: String,
-        default: '100%',
-      },
-      height: {
-        type:String,
-        default: '100%',
-      },
-    },
-    computed: {
-      variantClass() {
-        return `input-box--${this.variant}`
-      },
-    },
+
+<script>
+export default {
+text: String,
+props: {
+  id: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  placeholder: {
+    type: String,
+    default: 'Placeholder',
+  },
+  variant: {
+    type: String,
+    default: 'red',
+  },
+  width: {
+    type: String,
+    default: 'auto',
+  },
+  height: {
+    type:String,
+    default: 'auto',
+  },
+},
+computed: {
+  variantClass() {
+    return `input-box--${this.variant}`
+  },
+  boxStyle() {
+    const widthValue = parseFloat(this.width) || 0
+    const heightValue = parseFloat(this.height) || 0
+
+    const fontSize = (widthValue + heightValue) / 10
+
+    const paddingTopBottom = heightValue / 5
+    const paddingLeftRight = widthValue / 10
+
+    return {
+      width: this.width,
+      height: this.height,
+      fontSize: `${fontSize}px`,
+      padding: `${paddingTopBottom}px ${paddingLeftRight}px`, 
+    }
   }
-  </script>
-  
-  <style lang="scss" scoped>
+},
+}
+</script>
+
+<style lang="scss" scoped>
   .input-box {
-    width: var(--input-width);
-    height: var(--input-height);
-    padding: 16px;
-    border: 2px solid;
-    border-radius: 12px;
     font-family: "Inter", serif;
-  
-    &::placeholder {
-    color: #999;
-    opacity: 0.7;
-  }
-
-  &--red {
-    border: 0.2em $red;
-    border-radius: 1.2em;
-    border-style: solid;
-    outline: none;
-    font-weight: bold;
-    font-size: medium;
-    color: $red;
-    padding: 1.25em;
 
     &::placeholder {
+      color: #999;
+      opacity: 0.7;
+    }
+
+    &--red {
+      border: 0.2rem $red;
+      border-radius: 1.2rem;
+      border-style: solid;
+      padding: 1.25rem;
+      outline: none;
+      font-weight: bold;
+      font-size: medium;
       color: $red;
+
+      &::placeholder {
+        color: $red;
+      }
     }
-  }
 
-  &--green {
-    border: 0.2em $green;
-    border-radius: 1.25em;
-    border-style: solid;
-    outline: none;
-    font-weight: bold;
-    font-size: medium;
-    color: $green;
-    padding: 1.25em;
-
-    &::placeholder {
+    &--green {
+      border: 0.15rem $green;
+      border-radius: 1.25rem;
+      padding: 1.25rem;
+      border-style: solid;
+      outline: none;
+      font-weight: bold;
+      font-size: medium;
       color: $green;
+
+      &::placeholder {
+        color: $green;
+      }
     }
   }
-  }
-  
-  </style>
-  
+
+</style>
