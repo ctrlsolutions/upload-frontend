80d1fce0cfb538b21151b5e21fa569b63cd35e20
Erl <136324144+ermontano@users.noreply.github.com>
2025-02-13 14:07:43 +0800

    [Feat][Erl] Commit changes with buttons

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import './assets/main.css'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { BiCaretDownFill } from 'oh-vue-icons/icons'
+import { BiCaretDownFill,FcGoogle } from 'oh-vue-icons/icons'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-addIcons(BiCaretDownFill)
+addIcons(BiCaretDownFill,FcGoogle)
 
 const app = createApp(App)
 
