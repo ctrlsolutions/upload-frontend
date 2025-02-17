b98e5a0650b77fd7ed31e9296119e567a1ae0ed8
Lanieza <lblopez@up.edu.ph>
2025-02-17 10:57:25 +0800

    [feat][Lanie] Removed the comments in main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
-// import './assets/main.css'
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
-
-// import { OhVueIcon, addIcons } from 'oh-vue-icons'
-// import { FaCalendarAlt } from 'oh-vue-icons/icons'
-
-// addIcons(FaCalendarAlt)
-
-// const app = createApp(App)
-// app.use(router)
-// app.component('v-icon', OhVueIcon)
-// app.mount('#app')
-
-
 import './assets/main.css'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
 import { BiCaretDownFill, FcGoogle, FaCalendarAlt} from 'oh-vue-icons/icons'
