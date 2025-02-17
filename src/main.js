d719ea38a91b6e418c3c04ada0e3978cb85d78e2
Lanieza <lblopez@up.edu.ph>
2025-02-17 10:51:47 +0800

    [chore][Lanie] Reverts main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
+// import './assets/main.css'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// import { OhVueIcon, addIcons } from 'oh-vue-icons'
+// import { FaCalendarAlt } from 'oh-vue-icons/icons'
+
+// addIcons(FaCalendarAlt)
+
+// const app = createApp(App)
+// app.use(router)
+// app.component('v-icon', OhVueIcon)
+// app.mount('#app')
+
+
 import './assets/main.css'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { BiCaretDownFill, FcGoogle, FaCalendarAlt} from 'oh-vue-icons/icons'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { FaCalendarAlt } from 'oh-vue-icons/icons'
-
-addIcons(FaCalendarAlt)
+addIcons(BiCaretDownFill, FcGoogle, FaCalendarAlt)
 
 const app = createApp(App)
+
 app.use(router)
 app.component('v-icon', OhVueIcon)
 app.mount('#app')
